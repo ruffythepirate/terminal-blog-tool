8f88a132b8b873dacd47322421ed97052f7b81c2
ruffy <ruffy.developer@gmail.com>
2019-05-22 18:38:29 +0200

    Refine naming in blog_communicator.

--- blog_communicator.rb
+++ blog_communicator.rb
@@ -1,12 +1,12 @@
 class BlogCommunicator
 
-  def initialize(base_url, html_service)
-    @html_service = html_service
+  def initialize(base_url, http_service)
+    @http_service = http_service
     @base_url = base_url
   end
 
   def get_blog_posts
-    blog_items = @html_service.get(@base_url + "/api/user/post-list")
+    blog_items = @http_service.get(@base_url + "/api/user/post-list")
 
     blog_items.map { |item|  BlogPostSmall.from_hash(item)}
   end
